59e9c536420940696524b9798bf3a15815661317
AntimonyBullets <110652473+AntimonyBullets@users.noreply.github.com>
2023-10-24 09:28:54 +0000

    commit-3

--- quadraticformula.c
+++ quadraticformula.c
@@ -2,25 +2,31 @@
 #include <math.h>
 
 int main(){
-    int a,b,c;
+    float a,b,c;
     float r1,r2;
-    printf("Enter a:");
-    scanf("%d", &a);
-    printf("Enter b:");
-    scanf("%d", &b);
-    printf("Enter c:");
-    scanf("%d", &c);
+    printf("Enter a: ");
+    scanf("%f", &a);
+    printf("Enter b: ");
+    scanf("%f", &b);
+    printf("Enter c: ");
+    scanf("%f", &c);
 
-    float discriminant = b*b - 4*a*c;
+    float discriminant = (b*b) - (4*a*c);
 
-    if(discriminant > 0){
+    if(discriminant >= 0){
         r1 = (sqrt(discriminant) - b)/(2*a);
         r2 = (0 - sqrt(discriminant)- b)/(2*a);
         printf("The roots of the give equation are --> %f and %f", r1, r2);
     }
     else{
-        float comDiscriminant = -1 * discriminant;
-        printf("The roots are (-%f + %fi)/%d and (-%f + %fi)/%d", b, comDiscriminant, 2*a, b, comDiscriminant, 2*a);
+        float posDiscriminant = sqrt(-1 * discriminant)/(2*a);
+        float posB = -1*b/(2*a);
+        if(posB == 0){
+            printf("The roots are -%fi and %fi", posDiscriminant, posDiscriminant);
+        }
+        else{
+        printf("The roots are (%f - %fi) and (%f + %fi)", posB, posDiscriminant, posB, posDiscriminant);
+        }
     }
     return 0;
 }
